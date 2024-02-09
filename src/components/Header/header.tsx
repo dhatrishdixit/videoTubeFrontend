40fe11add953a635f0822d28d3865d098131b07f
dhatrishdixit <dhatrish29@gmail.com>
2024-02-09 10:50:26 +0530

    made headers better and more responsive

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,16 +1,6 @@
 import {Logo} from "../logo/logo"
-import {
-    NavigationMenu,
-    NavigationMenuContent,
-   // NavigationMenuIndicator,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    NavigationMenuTrigger,
-  //  NavigationMenuViewport,
-  } from "@/components/ui/navigation-menu"
-import { Input } from "../ui/input"
-import { ModeToggle, Switcher } from "../mood-toggle"
+
+import { Switcher } from "../mood-toggle"
 import {
   HoverCard,
   HoverCardContent,
@@ -19,34 +9,33 @@ import {
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { ScrollableArea } from "../ScrollableContent"
-import { Button } from "../ui/button"
 import { PublishedBtn } from "../publishVidBtn"
 import { InputSearch } from "../ui/inputSearch"
 
 export const Navbar = () =>{
     return (
       <div className=" px-2 flex w-full h-full items-center justify-around grid grid-cols-10">
-      {/* <Button variant="destructive">logout</Button> */}
+      
       <div className="col-span-3 flex ml-3 items-center gap-4"
            onClick={()=>{
-            // navigate to home page 
+            //TODO: navigate to home page 
            }}
       >
         <Logo/><p className="text-red-600 font-bold text-2xl">Video Tube</p>
         </div>
-      <div className="flex gap-8 items-center">
-        <div>
-        {/* <Input type="text" placeholder="search" className="w-[400px]"/> */}
-        <InputSearch className="w-[450px]"/>
+   
+        <div className="col-span-4">
+    
+        <InputSearch className="w-full"/>
         </div>
-        <div>
+        <div className="col-span-1">
           <PublishedBtn/>
         </div>
-        <div className="w-full">
+        <div className="col-span-1">
         <Switcher/>
         </div>
      
-        <div>
+        <div className="col-span-1 px-2 flex justify-center">
         <Avatar>
        <HoverCard>
        <HoverCardTrigger>
@@ -69,32 +58,8 @@ export const Navbar = () =>{
     
     
        </div>
-      </div>
+   
     )
 }
 
-     // write custom header with all these components
-  //    <NavigationMenu className="w-screen flex items-stretch">
-  //    <NavigationMenuList className="justify-between min-w-[800px]">
-  //      <NavigationMenuItem>
-  //        <Button variant="destructive">logout</Button>
-  //      </NavigationMenuItem>
-  //      <NavigationMenuItem>
-  //        <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-  //        <NavigationMenuContent>
-  //          <NavigationMenuLink> <Button variant="destructive">logout</Button></NavigationMenuLink>
-  //        </NavigationMenuContent>
-  //      </NavigationMenuItem>
-  //      <NavigationMenuItem>
-  //        <Input type="text" placeholder="search" className="w-[400px]"/>
-  //      </NavigationMenuItem>
-  //      <NavigationMenuItem>
-  //        <ModeToggle/>
-  //      </NavigationMenuItem>
-  //      <NavigationMenuItem>
-     
-  //      </NavigationMenuItem>
-     
-  //    </NavigationMenuList>
-  //  </NavigationMenu>
-   
+ 
