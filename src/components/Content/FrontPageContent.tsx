81f4251f5f8eb202745c2f3df1ce4b6cfd456dff
dhatrishdixit <dhatrish29@gmail.com>
2024-03-17 02:11:37 +0530

    removed lazy loading cause of some problems

--- src/components/Content/FrontPageContent.tsx
+++ src/components/Content/FrontPageContent.tsx
@@ -1,11 +1,18 @@
 import { SkeletonCardMain } from "../Card/skeletonCard";
 import { VideoCardMain } from "../Card/videoCard";
-import { useLoading } from "@/hooks/LazyLoading";
+
+import {useState} from "react";
+import { useToast } from "../ui/use-toast";
 
 export const ContentLoading = () => {
+  const {toast} = useToast() ;
+
+
+
   // check where we are using react router dom hooks and acccordingly use the div
   return (
     <div className="p-2 grid grid-cols-3 overflow-y-scroll h-[90vh] scrollbar-thin dark:scrollbar-track-[#09090b] scrollbar-thumb-red-600 scrollbar-track-white place-items-center">
+      {/* <SkeletonCardMain />
       <SkeletonCardMain />
       <SkeletonCardMain />
       <SkeletonCardMain />
@@ -13,12 +20,11 @@ export const ContentLoading = () => {
       <SkeletonCardMain />
       <SkeletonCardMain />
       <SkeletonCardMain />
-      <SkeletonCardMain />
-      <SkeletonCardMain />
+      <SkeletonCardMain /> */}
     
       {/* write conditional of loading here  */}
 
-      {/*          
+               
           <VideoCardMain />
           <VideoCardMain />
           <VideoCardMain />
@@ -28,7 +34,7 @@ export const ContentLoading = () => {
           <VideoCardMain />
           <VideoCardMain />
           <VideoCardMain />
-       */}
+      
     </div>
   );
 };
