afb7ebd8c0bd0cdbe76b1d0b58de2eacc265e616
dhatrishdixit <dhatrish29@gmail.com>
2024-04-04 18:41:05 +0530

    completed playlist card

--- src/components/Card/playlistCard.tsx
+++ src/components/Card/playlistCard.tsx
@@ -1,44 +1,56 @@
 import { useState } from "react";
 
-function stringShortener(str:string):string {
-
-     return str?.length >= 50 ? str?.substring(0,49) : str ;
-  }
-
-
-
-const props = {
-    video0:{
-        thumbnail:"https://res.cloudinary.com/dviowskng/image/upload/v1683962030/samples/ecommerce/leather-bag-gray.jpg",
-    },
-    video1:{
-        thumbnail:"https://res.cloudinary.com/dviowskng/image/upload/v1683962030/samples/ecommerce/car-interior-design.jpg"
-    },
-    video2:{
-        thumbnail:"https://res.cloudinary.com/dviowskng/image/upload/v1683962021/samples/ecommerce/analog-classic.jpg"
-    },
-    video3:{
-        thumbnail:"https://res.cloudinary.com/dviowskng/image/upload/v1683962033/samples/landscapes/nature-mountains.jpg"
-    },
-    createdAt: "3 days ago",
-    name:"my first playlist",
-    description:"this is my first playlist",
-    // owner:"65b63db59ea6a235c4b6ece1",
-    // channel:"dhatrish2",
-    // channelFullName:"sample1",
-    // channelAvatar:"http://res.cloudinary.com//image/upload/v1706442163/degk5k7iklrne5hg5cf4.png",
-    // views:0,
+function stringShortener(str: string): string {
+  return str?.length >= 50 ? str?.substring(0, 49) : str;
 }
 
-// blur transformation : https://res.cloudinary.com/dviowskng/prod/image/upload/e_blur:1500/me/v1706442163/degk5k7iklrne5hg5cf4.png
+const props = {
+  video0: {
+    thumbnail:
+      "https://res.cloudinary.com/dviowskng/image/upload/v1683962030/samples/ecommerce/leather-bag-gray.jpg",
+  },
+  video1: {
+    thumbnail:
+      "https://res.cloudinary.com/dviowskng/image/upload/v1683962030/samples/ecommerce/car-interior-design.jpg",
+  },
+  video2: {
+    thumbnail:
+      "https://res.cloudinary.com/dviowskng/image/upload/v1683962021/samples/ecommerce/analog-classic.jpg",
+  },
+  video3: {
+    thumbnail:
+      "https://res.cloudinary.com/dviowskng/image/upload/v1683962033/samples/landscapes/nature-mountains.jpg",
+  },
+  createdAt: "3 days ago",
+  name: "my first playlist",
+  description: "this is my first playlist",
+};
 
-// onHover make it darker 
+export function PlaylistCard() {
+  const [hover, setHover] = useState<boolean>(false);
 
-export function PlaylistCard(){
-    const [hover,setHover] = useState<boolean>(false);
-    return(
-        <div className="m-2">
-          <img src={props.video0.thumbnail}/>
+  return (
+    <div
+      className="m-2 relative"
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <img
+        src={
+          "https://res.cloudinary.com/dviowskng/image/upload/v1712173049/ehykw4gut4lrlfdwzinp.png" ||
+          props.video0.thumbnail
+        }
+        className={`w-[240px] h-[128px] transition-all duration-300 ${
+          hover ? "brightness-75 rounded-2xl blur-sm " : ""
+        }`}
+        alt="playlist card"
+      />
+      {hover && (
+        <div className="absolute inset-0 flex flex-col justify-between p-2 transition-opacity duration-300 cursor-pointer">
+          <p className="text-slate-200 font-semibold">{props.name}</p>
+          <p className="text-slate-200">{stringShortener(props.description)}</p>
         </div>
-    )
+      )}
+    </div>
+  );
 }
