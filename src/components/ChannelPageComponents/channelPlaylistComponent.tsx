7170aa3d1b5cdea766fc3126178bcdad511f95a6
dhatrishdixit <dhatrish29@gmail.com>
2024-04-07 02:57:50 +0530

    some small changes

--- src/components/ChannelPageComponents/channelPlaylistComponent.tsx
+++ src/components/ChannelPageComponents/channelPlaylistComponent.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { PlaylistCard } from '../Card/playlistCard'
 import {
   Pagination,
--- src/components/ChannelPageComponents/channelPostComponent.tsx
+++ src/components/ChannelPageComponents/channelPostComponent.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { TweetCard } from '../Card/TweetPostCard';
 import {
   Pagination,
